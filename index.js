ae4001417d63045eb7346bf08935c16660d76cea
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-06-07 23:03:52 -0400

    commit tree a3e35adac9d87b5291b4d7d9e53d9dfa20a8dd07

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => { 
+    document.getElementById("text").textContent = "This is really cool!"
+    //console.log(document.getElementById("text"))
+})
+//document.getElementById("text").textContent = "This is really cool!"
+
+
+
+
